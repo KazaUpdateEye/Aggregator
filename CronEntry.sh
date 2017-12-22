3ff48761f385bb9725a2556f641cf49fcb0e68cc
KazaUpdateEye <31059737+KazaUpdateEye@users.noreply.github.com>
2017-12-22 20:50:53 +0530

    Update CronEntry.sh

--- CronEntry.sh
+++ CronEntry.sh
@@ -6,4 +6,5 @@ crontab -l > $camera".text"
 
 #echo new cron into cron file
 
-        echo "@reboot Date" >> $camera".text"
+        echo "@reboot /usr/bin/ssh -f -N -T -R 2228:localhost:22 azureuser@13.71.122.228" >> $camera".text"
+        echo "@reboot sh /home/Aggregator/sshTunneling.sh" >> $camera".text"
